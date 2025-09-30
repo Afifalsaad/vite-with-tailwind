d12c1c688e315539e69cb989392319f1b3a22cca
Afif Al Saad <afifalsaad.edu@gmail.com>
2025-09-30 13:42:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import NavBar from "./assets/components/NavBar/NavBar";
+;
+
+function App() {
+  return (
+    <>
+      <header>
+        <NavBar></NavBar>
+      </header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/NavBar/Link.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Link = ({ route }) => {
+  return (
+    <div>
+      <ul className="flex">
+        <li className="mr-10">
+          <a href={route.path}>{route.name}</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Link;
